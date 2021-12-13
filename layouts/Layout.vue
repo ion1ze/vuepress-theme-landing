27e6590ced75af456a8178d43a2b05c9fa4f3200
focuxin <focuxin@qq.com>
2021-12-13 15:48:04 +0800

    添加备案

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -1,5 +1,20 @@
 <template>
-  <ParentLayout />
+  <div class="layout">
+    <ParentLayout />
+    <footer>
+      <p>
+        <a
+          v-if="config.footer.icp"
+          href="https://beian.miit.gov.cn/"
+          target="_blank"
+        >
+          {{ config.footer.icp || "" }}
+        </a>
+        {{ config.footer.police || "" }}
+      </p>
+      <p v-if="config.footer.copyright">&copy;{{ config.footer.copyright }}</p>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -9,8 +24,16 @@ export default {
   components: {
     ParentLayout,
   },
+  computed: {
+    config() {
+      return this.$site.themeConfig;
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="stylus">
+footer {
+  text-align: center;
+}
 </style>
